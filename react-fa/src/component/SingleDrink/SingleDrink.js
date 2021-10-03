78b50a014b2dabbfa96a1823cb5b18dcd8636dcd
gtr3fadi <86829039+gtr3fadi@users.noreply.github.com>
2021-10-03 03:42:39 -0700

    d
    d

--- react-fa/src/component/SingleDrink/SingleDrink.js
+++ react-fa/src/component/SingleDrink/SingleDrink.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class SingleDrink extends Component {
+    state = {
+        drink: null
+        
+    }
+
+
+
     // get the single drink from the props
     componentDidMount() {
         console.log(this.props.drink)
         let id = this.props.match.params.id
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        .then(res => {
+            console.log(res.data.drinks[0])
+            this.setState({
+                drink: res.data.drinks[0]
+        })
 
 
+    }
+    )
     }
 
 
@@ -15,7 +31,7 @@ class SingleDrink extends Component {
   render() {
     return (
       <>
-        
+        <h4>{this.state.drink.strDrink}</h4>
       </>
     )
   }
