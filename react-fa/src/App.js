0c55ef60cd144b8b70ad1af7ef8fd15ba85121e2
gtr3fadi <86829039+gtr3fadi@users.noreply.github.com>
2021-10-03 04:11:46 -0700

    d
    d

--- react-fa/src/App.js
+++ react-fa/src/App.js
@@ -86,28 +86,28 @@ import SingleDrink from './component/SingleDrink/SingleDrink';
         </switch> */}
 
         <Switch>
-          <Route  path="/" component={App} />
-          <Route path="/drink/:id" component={SingleDrink} />
+          {/* <Route  path="/" component={App} /> */}
+          <Route path="/drink/:SingleDrink_id" component={SingleDrink} />
         </Switch>
 
 
         
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
+          <Link className="navbar-brand" to="#">Navbar</Link>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav mr-auto">
               <li className="nav-item active">
-                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                <Link className="nav-link" to="#">Home <span className="sr-only">(current)</span></Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <Link className="nav-link" to="#">Link</Link>
               </li>
               
               <li className="nav-item">
-                <a className="nav-link " href="#">Disabled</a>
+                <Link className="nav-link " to="#">Disabled</Link>
               </li>
             </ul>
           </div>
--- react-fa/src/component/SingleDrink/SingleDrink.js
+++ react-fa/src/component/SingleDrink/SingleDrink.js
@@ -12,12 +12,13 @@ class SingleDrink extends Component {
     // get the single drink from the props
     componentDidMount() {
         console.log(this.props.drink)
-        let id = this.props.match.params.id
-        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+        let id = this.props.match.params.idDrink
+        axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${this.props.match.params.idDrink}`)
         .then(res => {
-            console.log(res.data.drinks[0])
+            console.log(res.data.drinks)
             this.setState({
-                drink: res.data.drinks[0]
+                drink: res.data.drinks
+
         })
 
 
@@ -29,12 +30,45 @@ class SingleDrink extends Component {
 
 
   render() {
+
+    const drink  = this.state.drink ? (
+        <div className="single-drink">
+            <h1>{this.state.drink.strDrink}</h1>
+            <img src={this.state.drink.strDrinkThumb} alt=""/>
+            <h3>Ingredients</h3>
+            <ul>
+                <li>{this.state.drink.strIngredient1}</li>
+                <li>{this.state.drink.strIngredient2}</li>
+                <li>{this.state.drink.strIngredient3}</li>
+                <li>{this.state.drink.strIngredient4}</li>
+                <li>{this.state.drink.strIngredient5}</li>
+                <li>{this.state.drink.strIngredient6}</li>
+                <li>{this.state.drink.strIngredient7}</li>
+                <li>{this.state.drink.strIngredient8}</li>
+                <li>{this.state.drink.strIngredient9}</li>
+                <li>{this.state.drink.strIngredient10}</li>
+                <li>{this.state.drink.strIngredient11}</li>
+                <li>{this.state.drink.strIngredient12}</li>
+                <li>{this.state.drink.strIngredient13}</li>
+                <li>{this.state.drink.strIngredient14}</li>
+                <li>{this.state.drink.strIngredient15}</li>
+            </ul>
+            <h3>Instructions</h3>
+            <p>{this.state.drink.strInstructions}</p>
+        </div>
+    ) : (
+        <div>Loading...</div>
+    )
+
     return (
-      <>
-        <h4>{this.state.drink.strDrink}</h4>
-      </>
+        <div>
+            {drink}
+        </div>
     )
   }
 }
+
+
+
 
 export default SingleDrink
